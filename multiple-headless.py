d61f4f4401edad2306d223388e2c099877e9f6b2
FPVogel <52160477+FPVogel@users.noreply.github.com>
2021-04-10 05:02:54 +0200

    Update License

--- multiple-headless.py
+++ multiple-headless.py
@@ -1,3 +1,19 @@
+# InstaditBot: Bot that fetches top subreddit posts and posts to instagram
+#  Copyright (C) 2021  Merlin Glander <merlinglander@vogelhosting.de A7CF F31A 48DF F3C6 0D19  C131 DC46 0EB0 3DBE 76C4>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import os
 import wget
