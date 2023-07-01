6cddb2e75a950d2c6e5596576cb4fe1e075cf3fd
Merlin <52160477+FPVogel@users.noreply.github.com>
2023-07-01 22:46:40 +0200

    Make multiple.py usable again

--- multiple.py
+++ multiple.py
@@ -21,82 +21,53 @@ import shutil
 import time
 import random
 import requests as r
-from instabot import Bot
+from instagrapi import Client
 from PIL import Image
 import threading
 
 # print license details
-print("InstaditBot  Copyright (C) 2021  Merlin Glander "
-      "<merlinglander@vogelhosting.de A7CF F31A 48DF F3C6 0D19  C131 DC46 0EB0 3DBE 76C4> \n"
-      " This program comes with ABSOLUTELY NO WARRANTY \n"
-      " This is free software, and you are welcome to redistribute it \n"
-      " under certain conditions")
+print("InstaditBot\n"
+      "This program comes with ABSOLUTELY NO WARRANTY\n"
+      "This is free software, and you are welcome to redistribute it\n"
+      "under certain conditions")
 
 # delete config folder
 if os.path.isdir("./config"):
     shutil.rmtree("./config")
 
-
 # load config dictionary
 if os.path.isfile("dictionary.txt"):
-    configFileR = open("dictionary.txt", "r")
-    config = json.load(configFileR)
-    configFileR.close()
+    with open("dictionary.txt", "r") as configFileR:
+        config = json.load(configFileR)
 else:
-    f = open("dictionary.txt", "w")
-    f.write(str("{}"))
-    f.close()
-    configFileR = open("dictionary.txt", "r")
-    config = json.load(configFileR)
-    configFileR.close()
-
+    with open("dictionary.txt", "w") as f:
+        f.write(str("{}"))
+    with open("dictionary.txt", "r") as configFileR:
+        config = json.load(configFileR)
 
 # add account to config
-def addaccount():
-    newusername = str(input("Username: "))
-    newpassword = str(input("Password: "))
-    newsubreddit = str(input("What Subreddit? (without /r/) "))
-    config.update({newusername: {}})
-    config[newusername].update({"username": newusername})
-    config[newusername].update({"password": newpassword})
-    config[newusername].update({"subreddit": newsubreddit})
+def add_account():
+    new_username = str(input("Username: "))
+    new_password = str(input("Password: "))
+    new_subreddit = str(input("What Subreddit? (without /r/) "))
+    config.update({new_username: {"username": new_username, "password": new_password, "subreddit": new_subreddit}})
     if input("Add another account?[y/n] ") == "y":
-        addaccount()
+        add_account()
     else:
         with open("dictionary.txt", "w") as configfilew:
             json.dump(config, configfilew)
 
-
 if input("Add new account?[y/n] ") == "y":
-    addaccount()
+    add_account()
 
-
-def createcaption(redditmemelist, hashtaglist, memelisti):
-    # get the description
-    hashtaglistcurrent = []
-    for _ in range(16):
-        randomhashtag = random.randint(0, len(hashtaglist) - 1)
-        hashtaglistcurrent.append(hashtaglist[randomhashtag])
-    return (
-        redditmemelist["data"]["children"][memelisti]["data"]["title"]
+def create_caption(reddit_memelist, hashtag_list, memelist_index):
+    hashtag_list_current = random.sample(hashtag_list, 16)
+    caption = (
+        reddit_memelist["data"]["children"][memelist_index]["data"]["title"]
         + "\n.\nWhy are you liking my memes but not following? \n.\n "
-        + hashtaglistcurrent[1]
-        + hashtaglistcurrent[2]
-        + hashtaglistcurrent[3]
-        + hashtaglistcurrent[4]
-        + hashtaglistcurrent[5]
-        + hashtaglistcurrent[6]
-        + hashtaglistcurrent[7]
-        + hashtaglistcurrent[8]
-        + hashtaglistcurrent[9]
-        + hashtaglistcurrent[10]
-        + hashtaglistcurrent[11]
-        + hashtaglistcurrent[12]
-        + hashtaglistcurrent[13]
-        + hashtaglistcurrent[14]
-        + hashtaglistcurrent[15]
+        + " ".join(hashtag_list_current)
     )
-
+    return caption
 
 # make images squares
 def make_square(im, min_size=256, fill_color=(0, 0, 0, 0)):
@@ -106,9 +77,8 @@ def make_square(im, min_size=256, fill_color=(0, 0, 0, 0)):
     new_im.paste(im, (int((size - sizex) / 2), int((size - sizey) / 2)))
     return new_im
 
-
 # download memelists
-def getmemelist(number):
+def get_memelist(number):
     currentsub = config[number]["subreddit"]
     downloadlink = "https://www.reddit.com/r/" + currentsub + "/hot.json?limit=99"
     memelist = r.get(downloadlink, headers={"User-Agent": "CIA-Datendiebstahl"}).content
@@ -116,30 +86,31 @@ def getmemelist(number):
         memelist = memelist.decode('utf-8')
         newjson.write(str(memelist))
     with open(currentsub + ".json", "r") as file:
-        redditmemelist = json.load(file)
-    return redditmemelist
-
+        reddit_memelist = json.load(file)
+    return reddit_memelist
 
 # log in to instagram
-def post(username, password, number):  # sourcery no-metrics
+def post(username, password, number):
     print("starting Thread " + str(number))
-    bot = Bot()
-    bot.login(username=username, password=password, is_threaded=True)
+    bot = Client()
+    bot.login(username=username, password=password)
     # call the download function
-    redditmemelist = getmemelist(number)
-    j = len(redditmemelist["data"]["children"])
+    reddit_memelist = get_memelist(number)
+    j = len(reddit_memelist["data"]["children"])
     # load posted files
-    if os.path.isfile(str(username) + "_posted.txt"):
-        with open(str(username) + "_posted.txt", "r") as postedlistfile:
+    postedlist_file = str(username) + "_posted.txt"
+    if os.path.isfile(postedlist_file):
+        with open(postedlist_file, "r") as postedlistfile:
             postedlist = postedlistfile.read().split('\n')
     else:
-        createnewposted = open(str(username) + "_posted.txt", "w")
-        createnewposted.close()
+        open(postedlist_file, "w").close()
+        postedlist = []
     # load hashtags
-    with open(username + "_hashtags.txt", "r") as hashtaglistFile:
-        hashtaglist = hashtaglistFile.read().split("\n")
-    for memelisti in range(j):
-        downlink = redditmemelist["data"]["children"][memelisti]["data"]["url"]
+    hashtag_list_file = username + "_hashtags.txt"
+    with open(hashtag_list_file, "r") as hashtaglistFile:
+        hashtag_list = hashtaglistFile.read().split("\n")
+    for memelist_index in range(j):
+        downlink = reddit_memelist["data"]["children"][memelist_index]["data"]["url"]
         extension = os.path.splitext(downlink)[1]
         filename = os.path.splitext(os.path.basename(downlink))[0]
         # make sure only jpgs come through
@@ -159,33 +130,26 @@ def post(username, password, number):  # sourcery no-metrics
                 new_image = make_square(test_image, fill_color=(255, 255, 255, 0))
                 new_image.save(filename + "_squared" + ".jpg")
                 # get the description
-                hashtaglistcurrent = []
-                for _ in range(16):
-                    randomhashtag = random.randint(0, len(hashtaglist) - 1)
-                    hashtaglistcurrent.append(hashtaglist[randomhashtag])
-                imagecaption = createcaption(redditmemelist, hashtaglist, memelisti)
+                image_caption = create_caption(reddit_memelist, hashtag_list, memelist_index)
                 # post to instagram
                 delay = random.randint(900, 1800)
                 print(" \n Waiting for " + str(delay) + " seconds")
                 time.sleep(delay)
-                bot.upload_photo(filename + "_squared.jpg", caption=imagecaption)
+                bot.photo_upload(filename + "_squared.jpg", caption=image_caption)
                 # add image to posted list
                 postedlist.append(filename)
-                with open(str(username) + "_posted.txt", "w") as postedlistfile:
-                    for element in postedlist:
-                        postedlistfile.write(element)
-                        postedlistfile.write('\n')
+                with open(postedlist_file, "w") as postedlistfile:
+                    postedlistfile.write('\n'.join(postedlist))
                 # count the yeah counter
                 os.remove(str(filename) + ".jpg")
-                os.remove(filename + "_squared" + ".jpg.REMOVE_ME")
+                os.remove(filename + "_squared" + ".jpg")
         else:
             print(username + ": Photo is not a .jpg, skipping!")
 
-
 # start threading with arguments
 for i in config:
-    currentusername = config[i]["username"]
-    currentpassword = config[i]["password"]
-    x = threading.Thread(target=post, args=(str(currentusername), str(currentpassword), i))
+    current_username = config[i]["username"]
+    current_password = config[i]["password"]
+    x = threading.Thread(target=post, args=(current_username, current_password, i))
     time.sleep(2)
     x.start()
